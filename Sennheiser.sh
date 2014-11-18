e89f240cef221afe381c3868e68bce392dfc6724
Sebastian Wagner <2000sw@gmail.com>
2014-11-18 18:49:24 +0100

    ignore streams already in correct sink

--- Sennheiser.sh
+++ Sennheiser.sh
@@ -29,17 +29,18 @@ SINK=$(pactl list short sinks | grep "$DEFAULT_SINKEXPRESSION" | head -n 1| cut 
 
 # #pactl list sink-inputs | grep -oP '(?<=Sink Input #|application.process.binary = ")[^"]*'
 # read streamid per application
-pactl list sink-inputs | grep -oP '(?<=Sink Input #|application.process.binary = "|Sink: )[^"]*' | while read STREAMID; do
+pactl list sink-inputs | grep -oP '(?<=Sink Input #|application.process.id = "|application.process.binary = "|Sink: )[^"]*' | while read STREAMID; do
  read OLDSINK
+ read PID
  read APPLICATION
  if [ -z "$APPLICATION" ]; then
     echo "no application for stream $STREAMID"
     exit 1
  fi
- if [ "$APPLICATION" == "$FILTER_APPLICATION" ]; then
-  echo "switching stream $STREAMID($APPLICATION) to sink $SINK(now $DEFAULT_SINKEXPRESSION was $OLDSINK)"
+ if [ "$APPLICATION" == "$FILTER_APPLICATION" -a "$SINK" != "$OLDSINK" ]; then
+  echo "switching stream $STREAMID($APPLICATION pid $PID) to sink $SINK(now $DEFAULT_SINKEXPRESSION was $OLDSINK)"
   pactl move-sink-input "$STREAMID" "$SINK"
  else
-  echo fail
+  echo "ignoring stream $STREAMID($APPLICATION pid $PID) from $SINK($DEFAULT_SINKEXPRESSION)"
  fi
 done
