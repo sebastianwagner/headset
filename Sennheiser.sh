87ec1f015f31cb0536067af5b83c4d2488ddb7f7
Sebastian Wagner <2000sw@gmail.com>
2014-11-18 18:38:54 +0100

    switch stream for chosen application

--- Sennheiser.sh
+++ Sennheiser.sh
@@ -1,2 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
+
+Sennheiser=`basename $0`
+
+USAGE=$(cat <<USAGE
+ #
+ # mkmageext.sh
+ # @author 2000sw@gmail.com
+ # @see https://unix.stackexchange.com/a/67398
+ #
+ $Sennheiser application [application]
+USAGE
+)
+
+if [ -z "$1" ]; then
+    echo "$USAGE"
+    exit 1
+fi
+
+DEFAULT_SINKEXPRESSION="Sennheiser"
+FILTER_APPLICATION="$1"
+
+#todo allow toggling
+#echo "looking for $FILTER_APPLICATION"
+
+# get sink id
+SINK=$(pactl list short sinks | grep "$DEFAULT_SINKEXPRESSION" | head -n 1| cut '-d	' -f1)
+
+# #pactl list sink-inputs | grep -oP '(?<=Sink Input #|application.process.binary = ")[^"]*'
+# read streamid per application
+pactl list sink-inputs | grep -oP '(?<=Sink Input #|application.process.binary = "|Sink: )[^"]*' | while read STREAMID; do
+ read OLDSINK
+ read APPLICATION
+ if [ -z "$APPLICATION" ]; then
+    echo "no application for stream $STREAMID"
+    exit 1
+ fi
+ if [ "$APPLICATION" == "$FILTER_APPLICATION" ]; then
+  echo "switching stream $STREAMID($APPLICATION) to sink $SINK(now $DEFAULT_SINKEXPRESSION was $OLDSINK)"
+  pactl move-sink-input "$STREAMID" "$SINK"
+ else
+  echo fail
+ fi
+done
